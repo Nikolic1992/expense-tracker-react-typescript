faff5b4294f91328fcf837bc5bb9bd693ef48a5d
Stevan Nikolic <stevannikolic1992@yahoo.com>
2025-06-07 13:28:48 +1000

    render ExpenseList and add simple delete logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+import ExpenseList from "./components/ExpenseList";
+
 function App() {
-  return <h1>Hello World</h1>;
+  const [expenses, setExpenses] = useState([
+    {
+      id: 1,
+      description: "Milk",
+      price: 10,
+      category: "Food",
+    },
+    {
+      id: 2,
+      description: "Meat",
+      price: 30,
+      category: "Food",
+    },
+    {
+      id: 3,
+      description: "Chicken",
+      price: 15,
+      category: "Food",
+    },
+  ]);
+  return (
+    <div>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
+    </div>
+  );
 }
 
 export default App;
